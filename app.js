ccd5764bbd74be0972b09151b0b23cf0e2ae2830
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-03-28 11:56:22 -0300

    examen terminado

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const main = async () => {
     console.log("Ingrese los siguientes datos: ");
 
     //pido los datos
-    const producto = await pedirDatos ("Producto: ");
-    const precio = await pedirDatos ("Precio: ");
-    const cantidad = await pedirDatos ("Cantidad: ");
+    const producto = await pedirDatos("Producto: ");
+    const precio = await pedirDatos("Precio: ");
+    const cantidad = await pedirDatos("Cantidad: ");
 
     rl.close();
     // console.log("---------------------------\nDatos ingresados:");
@@ -33,30 +33,51 @@ const main = async () => {
         nombre: producto,
         precio: precio,
         cantidad: cantidad,
-    }
+    };
 
     //pido el archivo json
     const argv = yargs.argv;
     const file = argv.file || argv.f || "productos.json";
 
-    fs.readFile(file, (err,data) => {
+    fs.readFile(file, "utf8", (err, data) => {
         let productos = [];
-        if (err) {
-            console.log("El archivo no existe, creando uno nuevo")
+
+        if (err || !data.trim()) {
+            console.log("el archivo no existe o está vacío- creando uno nuevo");
         } else {
-            productos = JSON.parse(data);
+            try {
+                productos = JSON.parse(data);
+            } catch (e) {
+                console.error("Error al leer el archivo JSON:", e);
+                return;
+            }
         }
         //se agrega el producto al array
         productos.push(nuevoProducto);
 
-        fs.writeFile(fila, JSON.stringify(productos), (err,data) => {
+        fs.writeFile(file, JSON.stringify(productos, null, 2), (err) => {
             if (err) {
                 console.error("error al guardar los cambios", err);
             } else {
                 console.log("se guardaron los productos");
+
+                // Lee el archivo JSON después de guardar los datos
+                fs.readFile(file, "utf8", (err, data) => {
+                    if (err) {
+                        console.error("Error al leer el archivo:", err);
+                    } else {
+                        console.log("Contenido del archivo:");
+                        try {
+                            const productosGuardados = JSON.parse(data);
+                            console.table(productosGuardados);
+                        } catch (e) {
+                            console.error("Error al leer el archivo ", e);
+                        }
+                    }
+                });
             }
         });
     });
-
 };
+
 main();
